ddfefd62a1ea2171fd6ef89f9c3902c2b8857b09
naresh <naresh.nalamolu@kony.com>
2016-07-05 13:21:51 +0530

    My 2nd version

---
+++ ServletDemo/src/com/kony/naresh/HelloServlet.java
@@ -0,0 +1,33 @@
+package com.kony.naresh;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet("/HelloServlet")
+public class HelloServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	 
+		response.setContentType("text/html");
+      		PrintWriter out = response.getWriter();
+			String Name = request.getParameter("name").trim();
+			out.println("hello "+Name);
+			out.flush();
+			out.close();
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+	}
+
+}
